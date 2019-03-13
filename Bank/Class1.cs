0c88c5fa3280c387f649c907272e52bfc9861b80
visualstudio <visualstudio@122-PC33>
2019-03-13 09:05:49 -0400

    unittest

--- Bank/Class1.cs
+++ Bank/Class1.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bank
+namespace BankAccountNS
 {
-    public class Class1
+    /// <summary>
+    /// Bank Account demo class.
+    /// </summary>
+    public class BankAccount
     {
+
     }
 }
